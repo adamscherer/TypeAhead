f2056847252051bc474445edc949ba0c2abb603e
Adam Scherer <adam@annconia.com>
2012-10-15 16:11:53 -0500

    Added remote implementation for Twitter

--- type-ahead-html/js/app.js
+++ type-ahead-html/js/app.js
@@ -11,6 +11,48 @@ define([
         typeahead.initialize();
     }
 
+    $.fn.typeahead.defaults = $.extend($.fn.typeahead.defaults, {
+        source: function (query, process) {
+            
+            if (this.timerId) {
+                clearTimeout(this.timerId);
+                this.timerId = null;
+            }
+            
+            // Cancel last call if already in progress
+            if (this.xhr) this.xhr.abort();
+            
+            var params = this.options.remote.preDispatch ? this.options.remote.preDispatch(query) : { query : query };
+            
+            this.timerId = setTimeout($.proxy(function() {
+                this.xhr = $.ajax({
+                    type: this.options.remote.method,
+                    url: this.options.remote.url,
+                    data: params,
+                    dataType: this.options.remote.dataType,
+                    success: function(data) {
+                        var items = [];
+                        
+                        $.each(data.values, function() {
+                            items.push(this.name);
+                        });
+                        
+                        process(items);
+                    }
+                  });
+                this.timerId = null;
+            }, this), this.options.remote.timeout);
+        },
+        remote: {
+            url: null,
+            timeout: 300,
+            method: 'post',
+            dataType: 'json',
+            preDispatch: null,
+            preProcess: null
+        }
+    });
+
     var typeahead = {
         initialize : function() {
             this.startListener('typeahead-city', 'http://api.type-ahead.com/city');
@@ -26,7 +68,7 @@ define([
                 e.preventDefault();
                 $this.typeahead({
                     items : 10,
-                    ajax : {
+                    remote : {
                         url : url,
                         dataType : 'jsonp',
                         triggerLength : 1,
