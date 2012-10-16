1d5ce7b2624209d704288cf4e7e825d105f1cd86
Mario Russo <mail.mr@gmail.com>
2012-10-15 21:12:56 -0400

    login/logout and creation of separate dashboard app(backbone). currently implemented as separate application, but we can possibly merge later once we thin out the static html pages

---
+++ type-ahead-html/js/views/dashboard/navigation.js
@@ -0,0 +1,90 @@
+define(
+    [
+        'jQuery',
+        'Underscore',
+        'Backbone',
+        'GlobalEvents',
+        'models/session',
+        'text!templates/dashboard/header.html',
+        'text!templates/dashboard/sidebar.html',
+        'text!templates/dashboard/footer.html'
+    ],
+    function($, _, Backbone, GlobalEvents, Session, HeaderTemplate, SidebarTemplate, FooterTemplate) {
+
+        var FooterView = Backbone.View.extend({
+            template : _.template(FooterTemplate),
+            render : function() {
+                this.$el.html(this.template());
+            },
+        });
+
+        var HeaderView = Backbone.View.extend({
+            template : _.template(HeaderTemplate),
+
+            events : {
+                "click #logout" : "logout",
+                "click .user-info" : "handleDropdown"
+            },
+            render : function() {
+                this.$el.html(this.template({
+                    data : Session.toJSON()
+                }));
+            },
+
+            logout : function(ev) {
+                Session.logout();
+
+                return false;
+            },
+
+            handleDropdown : function(ev) {
+                var active = $(ev.currentTarget);
+                var dropDown = $('.user-dropbox');
+                if (!active.hasClass('user-active')) {
+                    dropDown.slideDown('fast');
+                    active.addClass('user-active');
+                } else {
+                    active.removeClass('user-active');
+                    dropDown.hide();
+                }
+
+                $(document).one("click", function(ev) {
+                    active.removeClass('user-active');
+                    dropDown.hide();
+                });
+
+                return false;
+            }
+        });
+
+        var SidebarView = Backbone.View.extend({
+            template : _.template(SidebarTemplate),
+            initialize : function() {
+                this.render();
+                this.bindGlobalEvents();
+            },
+            render : function() {
+                this.$el.html(this.template());
+            },
+            bindGlobalEvents : function() {
+                var menuItems = this.$el.find('.leftmenu ul li');
+                GlobalEvents.bind('render:sidebar', function(selectedItem) {
+                    menuItems.each(function() {
+                        var item = $(this);
+                        item.removeClass('current');
+
+                        if (item.attr('id') === selectedItem) {
+                            item.addClass('current');
+                        }
+                    });
+                });
+            }
+        });
+
+        return {
+            header : HeaderView,
+            footer : FooterView,
+            sidebar : SidebarView
+        }
+    });
+    
---
+++ type-ahead-html/js/views/dashboard/profile.js
@@ -0,0 +1,21 @@
+define([
+    'require',
+    'jQuery',
+    'Underscore',
+    'Backbone',
+    'GlobalEvents',
+    'text!templates/dashboard/profile.html',
+], function(require, $, _, Backbone, GlobalEvents, MainTemplate) {
+
+    var View = Backbone.View.extend({
+        initialize : function() {
+            _.bindAll(this, "render");
+
+            this.$el.html(MainTemplate);
+        },
+        render : function(el) {
+        }
+    });
+
+    return View;
+});
